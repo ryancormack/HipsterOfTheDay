4e55f801a180dc4370ad02f698e1ac9f89ff19e4
ryancormack <ryancormack89@gmail.com>
2014-08-16 16:53:08 +0100

    Added Domain project and featureFolderCS file.

---
+++ HipsterOfTheDay/FeatureViewLocationRazorViewEngine .cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace HipsterOfTheDay
+{
+    public class FeatureViewLocationRazorViewEngine : RazorViewEngine
+    {
+        public FeatureViewLocationRazorViewEngine()
+        {
+            ViewLocationFormats = new[]
+            {
+                "~/Features/{1}/{0}.cshtml",
+                "~/Features/{1}/{0}.vbhtml",
+                "~/Features/Shared/{0}.cshtml",
+                "~/Features/Shared/{0}.vbhtml",
+            };
+
+            MasterLocationFormats = ViewLocationFormats;
+
+            PartialViewLocationFormats = new[]
+            {
+                "~/Features/{1}/{0}.cshtml",
+                "~/Features/{1}/{0}.vbhtml",
+                "~/Features/Shared/{0}.cshtml",
+                "~/Features/Shared/{0}.vbhtml",
+            };
+        }
+    }
+}
