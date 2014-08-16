ddf075405c3b5f8ba6c324c5a02451fd0fb23d7d
ryancormack <ryancormack89@gmail.com>
2014-08-16 16:59:39 +0100

    Added global.asax and Feature Folder viewengine

---
+++ HipsterOfTheDay/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HipsterOfTheDay
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new FeatureViewLocationRazorViewEngine());
+            //Do we need this?
+            //AreaRegistration.RegisterAllAreas();
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+
+    }
+}
