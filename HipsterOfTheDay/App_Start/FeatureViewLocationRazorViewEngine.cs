71eff4fc5c5ceb9f2b6418aa90eb68007eeb8d58
ryancormack <ryancormack89@gmail.com>
2014-08-25 13:42:06 +0100

    Initial commit. Changes from old github repo

---
+++ HipsterOfTheDay/App_Start/FeatureViewLocationRazorViewEngine.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace HipsterOfTheDay
+{
+    public class FeatureViewLocationRazorViewEngine : RazorViewEngine
+    {
+        public FeatureViewLocationRazorViewEngine()
+        {
+           var featureFolderViewLocationFormats = new[]
+            {
+                "~/Features/{1}/{0}.cshtml",
+                "~/Features/{1}/{0}.vbhtml",
+                "~/Features/Shared/{0}.cshtml",
+                "~/Features/Shared/{0}.vbhtml",
+            };
+
+            ViewLocationFormats = featureFolderViewLocationFormats;
+            MasterLocationFormats = featureFolderViewLocationFormats;
+            PartialViewLocationFormats = featureFolderViewLocationFormats;
+        }
+    }
+}
---
+++ HipsterOfTheDay/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HipsterOfTheDay
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new FeatureViewLocationRazorViewEngine());
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();
+        }
+
+    }
+}
