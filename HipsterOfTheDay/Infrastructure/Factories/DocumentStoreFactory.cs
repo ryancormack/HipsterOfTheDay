6e3ec4b16cae8bdbe69233ab319fdaafa5447873
ryancosans <ryanmcosans@gmail.com>
2014-09-04 10:57:54 +0100

    caslte and raven document store added

---
+++ HipsterOfTheDay/Infrastructure/Factories/DocumentStoreFactory.cs
@@ -0,0 +1,23 @@
+﻿using Castle.MicroKernel;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace HipsterOfTheDay.Infrastructure.Factories
+{
+    public static class DocumentStoreFactory
+    {
+        public static IDocumentStore CreateDocumentStore()
+        {
+            var documentStore = new DocumentStore { ConnectionStringName = "HipsterHQ" };
+            documentStore.Initialize();
+            //IndexCreation.CreateIndexes(typeof(Post).Assembly, store);
+            return documentStore;
+        }
+
+        public static IDocumentSession GetDocumentSession(IKernel kernel)
+        {
+            var store = kernel.Resolve<IDocumentStore>();
+            return store.OpenSession();
+        }
+    }
+}
