784f47ef67bf6a3ce77187ad145a5f96848bf48a
ryancosans <ryanmcosans@gmail.com>
2014-09-10 17:43:57 +0100

    Cosans didn't check shit in. Derp. Excluded web/model/imagecs

---
+++ HispterOfTheDay.Domain/Model/Image.cs
@@ -0,0 +1,34 @@
+﻿namespace HispterOfTheDay.Domain.Model
+{
+    public class Image
+    {
+        protected bool Equals(Image other)
+        {
+            return string.Equals(ImageData, other.ImageData);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((Image)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (ImageData != null ? ImageData.GetHashCode() : 0);
+        }
+
+        public string ImageData { get; set; }
+    }
+}
